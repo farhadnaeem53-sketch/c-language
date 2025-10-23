4cb097a4a9fb42708541bc6b5e818984af81134d
Farhad Naeem <farhadnaeem53@gmail.com>
2025-10-23 17:28:00 +0500

    added new file

---
+++ ctuts7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    a = 34;
+    b = 36;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+
+    return 0;
+}
