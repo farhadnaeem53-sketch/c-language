597d10af2c8f58dc24f5a246a66cdefe0f3c37c8
Farhad Naeem <farhadnaeem53@gmail.com>
2025-10-27 15:07:22 +0500

    First commit

--- ex1.c
+++ ex1.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+/*
+print multiplication table of a number entered by the user in p[retty form]
+
+example:
+
+input
+Enter the number you want multiplication table of:
+6
+
+output:
+table of 6:
+6 x 1 = 6
+6 x 2 = 12
+6 x 3 = 18 
+6 x 4 = 24
+6 x 5 = 30
+6 x 6 = 36
+6 x 7 = 42 
+6 x 8 = 48
+6 x 9 = 54 
+6 x 10 = 60
+*/
+
+
 int main()
 {
     /* code */
