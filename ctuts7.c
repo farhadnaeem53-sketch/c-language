6beda7036b9af18dbe2f081fb339bb390a23fa60
Farhad Naeem <farhadnaeem53@gmail.com>
2025-10-27 14:24:39 +0500

    First commit

--- ctuts7.c
+++ ctuts7.c
@@ -3,10 +3,10 @@
 int main(int argc, char const *argv[])
 {
     int a, b;
-    a = 34;
-    b = 36;
+    a = 2;
+    b = 3;
 
-    printf("a + b = %d\n", a + b);
+    printf("a || b = %d\n", a&b);
     printf("a - b = %d\n", a - b);
     printf("a * b = %d\n", a * b);
     printf("a / b = %d\n", a / b);
