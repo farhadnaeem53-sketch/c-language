d5a95d37beee3ee86826f37cd9a2cf0eaa20d627
Farhad Naeem <farhadnaeem53@gmail.com>
2025-10-28 19:03:34 +0500

    first commit

--- ex2.c
+++ ex2.c
@@ -4,8 +4,10 @@ int main()
 {
     int a =8;
     float b = 7.333;
-     printf("Hello World\n");
-     printf("The value of a is %d and the value of b is %.4f\n", a, b);
+    // printf("Hello World\n");
+     //printf("The value of a is %d and the value of b is %.4f\n", a, b);
 
+
+     
     return 0;
 }
