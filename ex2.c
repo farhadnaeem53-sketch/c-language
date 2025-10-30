392c1a098880245f37c286bce768927a4e5401ff
Farhad Naeem <farhadnaeem53@gmail.com>
2025-10-30 17:34:16 +0500

    first commit

--- ex2.c
+++ ex2.c
@@ -4,8 +4,9 @@ int main()
 {
     int a =8;
     const float b = 7.333;
-    printf("%f", PI);
-    // b = 7.22;  // Since b is a constant
+    //PI = 7.33;   //Cannot do this since PI is a constant
+    printf("my backslash \\ %f", PI);
+    // b = 7.22;  //Cannot do this since b is a constant
     // printf("Hello World\n");
     //printf("The value of a is %d and the value of b is %.4f\n", a, b);
 
