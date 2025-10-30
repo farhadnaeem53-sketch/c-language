93ac80dca53b99c1535156d47b5449ff7952dff4
Farhad Naeem <farhadnaeem53@gmail.com>
2025-10-30 16:04:30 +0500

    first commit

--- ex2.c
+++ ex2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+#define PI 3.14
 int main()
 {
     int a =8;
     const float b = 7.333;
+    printf("%f", PI);
     // b = 7.22;  // Since b is a constant
     // printf("Hello World\n");
     //printf("The value of a is %d and the value of b is %.4f\n", a, b);
