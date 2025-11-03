ae2712cf337797c52be6498cb4512385cc8167dc
Farhad Naeem <farhadnaeem53@gmail.com>
2025-11-03 15:31:21 +0500

    First commit

--- ex4.c
+++ ex4.c
@@ -2,15 +2,27 @@
 
 int main()
 {
-    int age;
+    int age, marks;
     printf("Enter your age\n");
     scanf("%d", &age);
 
+printf("Enter your marks\n");
+    scanf("%d", &marks);
     switch (age)
     {
         case 3:
             printf("The age is 3\n");
-           // break;
+            switch (marks)
+            {
+            case 45:
+                printf("your marks are 45");
+                break;
+            
+            default:
+                    printf("Your marks are not 45");
+                break;
+            }
+            break;
 
         case 13:
             printf("The age is 13\n");
