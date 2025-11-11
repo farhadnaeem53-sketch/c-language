7c6ca89382513c6b98ea717d1db0109c4bcda0f0
Farhad Naeem <farhadnaeem53@gmail.com>
2025-11-11 15:46:25 +0500

    First commit

---
+++ ex7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+     printf("Hello World\n");
+     int j,i;
+     for ( i = 0, j = 0; i < 5; i++)
+     {
+        printf("%d %d\n", i, j);
+     }
+     
+    return 0;
+}
