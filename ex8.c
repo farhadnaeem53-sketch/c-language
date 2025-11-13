d4c46008a356eb3925c0ba160e28704dc909b65f
Farhad Naeem <farhadnaeem53@gmail.com>
2025-11-13 17:19:22 +0500

    First commit

---
+++ ex8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+     printf("Hello World\n");
+     int i, age;
+    for(i=0; i<10;i++)
+    {
+    printf("%d\nEnter you age", i)
+    scanf("%d", &age);
+    if(age>10)
+    {
+        break;
+    }
+    }
+    return 0;
+}
