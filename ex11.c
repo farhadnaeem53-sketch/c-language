84a444ce8c787bc45b278b674e454bf91eb13458
Farhad Naeem <farhadnaeem53@gmail.com>
2025-12-23 12:08:40 +0500

    update

--- ex11.c
+++ ex11.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
+// function declaration
+int sum(int a, int b);
+void printstar(int n);
+
+// main function
 int main()
 {
-     printf("Hello World\n");
+    int a, b, c;
+
+    a = 9;
+    b = 87;
+
+    c = sum(a, b);
+    printf("The sum is %d \n", c);
+
+    printstar(5);
+
     return 0;
+}
+
+// function definition
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+// function definition
+void printstar(int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
 }
